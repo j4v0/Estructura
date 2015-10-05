fd13bdb854e37c99db2d878d274e3c3c800799ec
J4v0 <jschunk@gmail.com>
2015-10-05 16:14:56 -0300

    Creación de clases

--- Galaxia.py
+++ Galaxia.py
@@ -6,8 +6,28 @@ import random
 
 pilas = pilasengine.iniciar()
 
-#Creando la nave
-nave = pilas.actores.Nave(0, -100)
+#Creación de Menú
+pilas.fondos.Noche()
+
+class Bienvenida(pilasengine.escenas.Escena):
+	def iniciar_juego():
+		print "Tengo que iniciar el juego"
+		Escena1_juego
+	
+	def salir_del_juego():
+		print "Tengo que salir..."
+		pilas.terminar()
+	
+	pilas.actores.Menu(
+		[
+			("Iniciar Juego", iniciar_juego),
+			("Salir del Juego", salir_del_juego),
+		])
+
+class Escena1_juego(pilasengine.escenas.Escena):
+	def iniciar(self):
+		#Creando la nave
+		nave = pilas.actores.Nave(0, -100)
 
 #Creando los asteroides
 enemigos = pilas.actores.Piedra()*8
