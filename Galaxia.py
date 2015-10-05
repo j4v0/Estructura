8bf5a4967fa7278a57f90490eb8bda291b781e68
J4v0 <jschunk@gmail.com>
2015-10-05 01:17:29 -0300

    Videojuego para Estructura de Datos IFTS N°18 año 2015
    Signed-off-by: J4v0 <jschunk@gmail.com>

---
+++ Galaxia.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import pilasengine
+import random
+
+pilas = pilasengine.iniciar()
+
+#Creando la nave
+nave = pilas.actores.Nave()
+
+#Creando los asteroides
+enemigos = pilas.actores.Piedra() * 10
