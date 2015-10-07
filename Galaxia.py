03614ddde8d37bd27635b1c28166250c61fe9358
j4v0 <jschunk@gmail.com>
2015-10-07 02:13:56 -0300

    Cambios varios

--- Galaxia.py
+++ Galaxia.py
@@ -11,13 +11,13 @@ pilas = pilasengine.iniciar()
 
 class Bienvenida(pilasengine.escenas.Escena):
 	pilas.fondos.Noche()
-	titulo = pilas.actores.Texto("EXPLOSION GALACTICA", magnitud=40, fuente="visitor1.ttf", y=90)	
-	def iniciar():
-		print "Tengo que iniciar el juego"
-		pilas.fondos.Noche()
+	pilas.actores.Texto("EXPLOSION GALACTICA", 
+						magnitud=40, fuente="visitor1.ttf", y=90)	
 	
-	def _iniciar_juego():
-		pilas.escenas.Escena1_juego()
+	def iniciar_juego():
+		print "Tengo que iniciar el juego"
+		pilas.escenas.Escena1()
+		
 	
 	def salir_del_juego():
 		print "Tengo que salir..."
@@ -25,19 +25,30 @@ class Bienvenida(pilasengine.escenas.Escena):
 	
 	pilas.actores.Menu(
 		[
-			("Iniciar Juego", _iniciar_juego),
+			("Iniciar Juego", iniciar_juego),
 			("Salir del Juego", salir_del_juego),
 		])
 		
-class Escena1_juego(pilasengine.escenas.Escena):
-	def _iniciar_(self):
+class Escena1(pilasengine.escenas.Escena):
+	def iniciar(self):
+		self.pilas.fondos.Galaxia()
 		#Creando la nave
-		nave = pilas.actores.Nave(0, -100)
+		self.nave = pilas.actores.Nave(0, -100)
+		self.puntos = pilas.actores.Puntaje(x=230, y=200,
+                                            color=pilas.colores.amarillo)
+		self.pilas.actores.Sonido()
 		nave.aprender(pilas.habilidades.LimitadoABordesDePantalla)
 		nave.definir_enemigos(enemigos, puntaje.aumentar)
 		pilas.colisiones.agregar(nave, enemigos, nave.eliminar)
 		#Creando los asteroides
 		enemigos = pilas.actores.Piedra()*12
 		nave.definir_enemigos(enemigos)
+		
+pilas.escenas.vincular(Bienvenida)
+pilas.escenas.vincular(Escena1)
+pilas.escenas.vincular(EscenaFin)
+pilas.escenas.Bienvenida
+
+
 
 pilas.ejecutar()
